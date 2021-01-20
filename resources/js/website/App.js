8cf5ef596b54e23fcc713da3be0e03ccdffec58d
luchetu <humluchetu@gmail.com>
2021-01-20 09:32:09 +0300

    added TaskList component to display the tasks

--- resources/js/website/App.js
+++ resources/js/website/App.js
@@ -1,21 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-export default class App extends Component {
-    render() {
-        return (
-            <div className="container-fluid">
-                <div className="row justify-content-center">
-                    <div className="col-md-8">
-                        <div className="card">
-                            <div className="card-header">Hello</div>
-                            <div className="card-body">I'm the website</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
-    }
-}
-if (document.getElementById('app')) {
-    ReactDOM.render(<App />, document.getElementById('app'));
-}
+import Index from './components/Index';
+
+ReactDOM.render(<Index />, document.getElementById('root'));
